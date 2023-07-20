fb98128b295c0a67172616c2f72fb65a45f5324d
Dmitriy Mayorov <MayorDi@yandex.ru>
2023-07-20 13:32:06 +0300

    Add impl GetPosition.

--- libs/macro_tools_pd/src/lib.rs
+++ libs/macro_tools_pd/src/lib.rs
@@ -15,3 +15,23 @@ fn impl_arr_pos_macro(ast: &syn::DeriveInput) -> TokenStream {
     };
     gen.into()
 }
+
+#[proc_macro_derive(GetPosition)]
+pub fn get_pos(input: TokenStream) -> TokenStream {
+    let ast = syn::parse(input).unwrap();
+
+    impl_get_pos_macro(&ast)
+}
+
+fn impl_get_pos_macro(ast: &syn::DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+    let gen = quote! {
+        impl GetPosition for #name {
+            fn get_position(&self) -> Position {
+                self.position
+            }
+        }
+    };
+
+    gen.into()
+}
